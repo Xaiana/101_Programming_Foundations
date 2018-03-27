c0f33b84f33184dea5a1019adb9d3545f33ca458
Xaiana <melisskann@gmail.com>
2018-03-27 16:52:16 -0700

    choose 5

--- lesson_6/tictactoe.rb
+++ lesson_6/tictactoe.rb
@@ -64,17 +64,29 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
-def find_at_risk_square(line, board, marker)
-  if board.values_at(*line).count(marker) == 2
-    board.select{|k,v| line.include?(k) && v == INITIAL_MARKER}.keys.first
+def find_at_risk_square(line, brd, marker)
+  if brd.values_at(*line).count(marker) == 2
+    brd.select{|k,v| line.include?(k) && v == INITIAL_MARKER}.keys.first
   else
     nil
   end
 end
 
-def computer_places_piece!(brd)
-  if brd[5] == INITIAL_MARKER
+def computer_choose_5(line, brd)
+  square = nil
+  if brd.select{|k,v| line.include?(k == 5) && v == INITIAL_MARKER}
     square = brd[5]
+  else
+    square = empty_squares(brd).sample
+  end
+end
+
+def computer_places_piece!(brd)
+  square = nil
+
+  WINNING_LINES.each do |line|
+    square = computer_choose_5(line, brd)
+    break if square
   end
 
   # offense
@@ -85,7 +97,7 @@ def computer_places_piece!(brd)
     end
   end
 
-  # defense
+  # defense first
   if !square
     WINNING_LINES.each do |line|
       square = find_at_risk_square(line, brd, PLAYER_MARKER)
@@ -136,7 +148,6 @@ loop do # main loop
     computer_places_piece!(board)
     break if someone_won?(board) || board_full?(board)
   end
-
   display_board(board)
 
   loop do
